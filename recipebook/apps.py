dc9a3f0343dd6243545c43ef69ad9ac47e09c0b4
vlad boiko <mail@vladboiko.ru>
2022-02-07 18:36:35 +0300

    added app verbose name

--- recipebook/apps.py
+++ recipebook/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class RecipebookConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipebook'
+    verbose_name = "Книга рецептов"
+
