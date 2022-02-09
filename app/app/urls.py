42b4016aa25e8e2a97c2fe82a45a1bc334326406
vlad boiko <mail@vladboiko.ru>
2022-02-09 19:11:11 +0300

    Restructured project and added docker

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,4 +1,4 @@
-"""django_recipebook URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path, include
 
-from . import views
 
 urlpatterns = [
-    path('', views.RecipeListView.as_view(), name="recipe_list"),
-    path("filter/", views.FilterRecipeView.as_view(), name="filter"),
-    path("search/", views.Search.as_view(), name="search"),
-    path('<slug:slug>/', views.RecipeDetailView.as_view(), name="recipe_detail"),
+    path('admin/', admin.site.urls),
+    path('', include('recipebook.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- app/recipebook/urls.py
+++ app/recipebook/urls.py
@@ -1,4 +1,4 @@
-"""django_recipebook URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,17 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path, include
 
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('recipebook.urls')),
+    path('', views.RecipeListView.as_view(), name="recipe_list"),
+    path("filter/", views.FilterRecipeView.as_view(), name="filter"),
+    path("search/", views.Search.as_view(), name="search"),
+    path('<slug:slug>/', views.RecipeDetailView.as_view(), name="recipe_detail"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
